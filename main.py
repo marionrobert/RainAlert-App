554a3fe345155d7c1d225b76357c54dce28a1869
Marion <mrobert148@gmail.com>
2023-03-08 18:48:59 +0100

    adding environment variables

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
 from twilio.rest import Client
+import os
+# line for pythonanywhere to connect to proxy server
+# from twilio.http.http_client import TwilioHttpClient
 
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
-API_KEY = "ac7e8f309ee54b11ec52b72fb0396f04"
+API_KEY = os.environ["OWM_API_KEY"]
 LAT = 48.691280
 LONG = 2.376180
 weather_params = {
@@ -13,7 +16,10 @@ weather_params = {
     "exclude": "alerts,daily,current,minutely"
 }
 
-
+account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+twilio_phone_number = os.environ["TWILIO_PHONE_NUMBER"]
+my_phone_number = os.environ["MY_PHONE_NUMBER"]
 
 response = requests.get(OWM_Endpoint, params=weather_params)
 response.raise_for_status()
@@ -29,11 +35,14 @@ for id in all_ids:
         will_rain = True
 
 if will_rain:
-    print("Bring an umbrella")
+    # add the 3 lines below for pythonanywhere to connect to proxy server
+    # proxy_client = TwilioHttpClient()
+    # proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+    # client = Client(account_sid, auth_token, http_client=proxy_client)
     client = Client(account_sid, auth_token)
     message = client.messages.create(
         body="It's going to rain today. Remember to bring an umbrella.",
-        from_=test_phone_number,
+        from_=twilio_phone_number,
         to=my_phone_number
     )
     print(message.status)
